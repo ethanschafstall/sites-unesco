38645c60f10f729bde9a50d1e2cb8887979c25a5
ethanschafstall <ethans9999@gmail.com>
2024-01-22 15:20:51 +0100

    changed map script file extension

--- Site/src/javascript/scriptMap.mjs
+++ Site/src/javascript/scriptMap.mjs
@@ -3,6 +3,8 @@
 // Date : 15.01.2024
 // Description : Map functionality code
 
+import { allMarkUnesco } from "./scriptMarker.mjs";
+
 // Map parameters (starting position, zoom amount).
 let mapOptions = {
     center:[51.958, 9.141],
@@ -10,14 +12,10 @@ let mapOptions = {
 }
 
 // Object for creating the map.
-let map = new L.map('map' , mapOptions);
+const map = new L.map('map' , mapOptions);
 
 // The different layers of the map.
 let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 map.addLayer(layer);
 
-// Marker object with cords.
-let marker = new L.Marker([51.958, 9.141]);
-
-// Add the marker to the map.
-marker.addTo(map);
+allMarkUnesco(map);
